1bae735dc965e186d5cba04edc594b369eced965
Matthew DeGennaro <mattdeeg@gmail.com>
2015-08-25 09:08:35 -0400

    Removing specific example for generic example

---
+++ exercises/exercise_example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  initial: function () {
+    // Initial code to be served to the participants
+  },
+  tests: function(code) {
+    // Unit tests that will be run against their code
+    // These will be visible to all participants
+    // Tests should use jasmine with chai
+  }
+};
