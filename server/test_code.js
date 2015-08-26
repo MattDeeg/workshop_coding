f6f6f7566edb600738fecf7c3b407d698a8cd450
Matt DeGennaro <mdegennaro@wayfair.com>
2015-08-26 11:42:58 -0400

    Changing test runner from jasmine-node to jasmine and simplifying reporter

--- server/test_code.js
+++ server/test_code.js
@@ -1,68 +1,33 @@
 var _ = require('underscore');
-var jasmine = require('jasmine-node');
+var Jasmine = require('jasmine');
 var chai = require('chai');
 var WorkshopReporter = require('./workshop_reporter');
 chai.should();
 
 module.exports = function(code, tests, callback) {
-  var reporter = new WorkshopReporter({
-    onComplete: function() {
-      var barHtml = '';
-      var output = reporter.console_;
-      if (output.length) {
-        output += '<hr>';
-      }
-      var numTests = 0;
-      var numTestsPassing = 0;
-      _.each(reporter.specMap_, function(spec) {
-        var numErrors = spec.errors.length;
-        numTests += 1;
+  var reporter = new WorkshopReporter();
 
-        var className = numErrors === 0 ? 'pass' : 'fail';
-        barHtml += '<span class="bar ' + className + '"></span>';
-
-        if (numErrors > 0) {
-          output += '<div class="failing">' + spec.name + '</div>';
-          for (var i = 0; i < numErrors; i++) {
-           output += '<div class="failing-error">' + spec.errors[i].message + '</div>';
-          }
-        } else {
-          numTestsPassing += 1;
-          output += '<div class="passing">' + spec.name + '</div>';
-        }
-      });
-      output += '<div class="pb"></div>';
-
-      var testPercent = numTestsPassing / numTests;
-      var testClass = 'poor';
-      if (testPercent > 0.85) {
-        testClass = 'great';
-      } else if (testPercent > 0.5) {
-        testClass = 'good';
-      }
-      callback({
-        testsStr: numTestsPassing + '/' + numTests,
-        testClass: testClass,
-        barHtml: barHtml,
-        output: output
-      });
-    }
+  var jasmine = new Jasmine();
+  jasmine.addReporter(reporter);
+  jasmine.onComplete(function() {
+    callback({
+      testsStr: reporter.testsStr,
+      testClass: reporter.testClass,
+      barHtml: reporter.barHtml,
+      output: reporter.consoleOutput + '<hr>' + reporter.testOutput
+    });
   });
-
-  var jasmineEnv = jasmine.getEnv();
-  jasmineEnv.currentRunner_ = new jasmine.Runner(jasmineEnv);
-  jasmineEnv.addReporter(reporter);
 
   // Yea yea this is an eval, but whachagonnado?
   try {
     new Function('var __jfn=function(console){' + code + '\n\n' + tests + '};__jfn.apply(this, arguments)')(reporter.console); // jshint ignore:line
+    jasmine.execute();
   } catch (ex) {
-    console.log(ex);
     callback({
-      progressMap: [],
-      output: ex.message
+      testsStr: 'error',
+      testClass: 'poor',
+      barHtml: '<span class="bar fail"></span>',
+      output: '<div class="failing">' + ex.message + '</div>'
     }, false);
   }
-  jasmineEnv.execute();
-
 };
--- server/workshop_reporter.js
+++ server/workshop_reporter.js
@@ -18,138 +18,66 @@ function stringifyArgs(args) {
 
 function consoleFn() {
   var args = stringifyArgs(arguments);
-  this.console_ += '<div class="message">';
-  this.console_ += args.length === 1 ? args[0] : ('[' + args.join(', ') + ']');
-  this.console_ += '</div>';
+  this.consoleOutput += '<div class="message">';
+  this.consoleOutput += args.length === 1 ? args[0] : ('[' + args.join(', ') + ']');
+  this.consoleOutput += '</div>';
 }
 
-var WorkshopReporter = function(config) {
-  config = config || {};
-  var self = this;
-  this.print_ = config.print || function (str) { self.output += str.toString(); };
-
-  this.started_ = false;
-  this.finished_ = false;
-
+var WorkshopReporter = function() {
   var boundConsoleFn = _.bind(consoleFn, this);
   this.console = {
     log: boundConsoleFn,
     dir: boundConsoleFn
   };
 
-  this.callback_ = config.onComplete || false;
-  this.console_ = '';
-  this.specMap_ = {};
-  this.suites_ = [];
-  this.specResults_ = {};
-  this.failures_ = [];
-  this.numSpecs_ = 0;
-  this.numFailingSpecs_ = 0;
-  this.includeStackTrace_ = config.includeStackTrace === false ? false : true;
-  this.stackFilter_ = config.stackFilter || function(t) { return t; };
+  this.consoleOutput = '';
+  this.testOutput = '';
+
+  this.specMap = {};
+  this.suites = [];
+  this.numSpecs = 0;
+  this.numPassingSpecs = 0;
+  this.barHtml = '';
 };
-
-
 WorkshopReporter.prototype = {
-  reportRunnerStarting: function(runner) {
-    this.started_ = true;
-    this.startedAt = new Date();
-    var suites = runner.topLevelSuites();
-    for (var i = 0; i < suites.length; i++) {
-      var suite = suites[i];
-      this.suites_.push(this.summarize_(suite));
-    }
+  suiteStarted: function(result) {
+    this.testOutput += '<div class="suite">' + result.description + ':</div>';
   },
 
-  summarize_: function(suiteOrSpec) {
-    var isSuite = suiteOrSpec instanceof jasmine.Suite;
+  specStarted: function(spec) {
+    this.numSpecs += 1;
+  },
 
-    // We could use a separate object for suite and spec
-    var summary = {
-      id: suiteOrSpec.id,
-      name: suiteOrSpec.description,
-      type: isSuite? 'suite' : 'spec',
-      suiteNestingLevel: 0,
-      children: [],
-      errors: []
-    };
+  specDone: function(result) {
+    var numErrors = result.failedExpectations.length;
 
-    if (!isSuite) {
-      this.specMap_[summary.name] = summary;
-      this.numSpecs_ += 1;
+    var className = numErrors === 0 ? 'pass' : 'fail';
+    if (numErrors === 0) {
+      this.numPassingSpecs += 1;
     }
+    this.barHtml += '<span class="bar ' + className + '"></span>';
 
-    if (isSuite) {
-      var calculateNestingLevel = function(examinedSuite) {
-        var nestingLevel = 0;
-        while (examinedSuite.parentSuite !== null) {
-          nestingLevel += 1;
-          examinedSuite = examinedSuite.parentSuite;
-        }
-        return nestingLevel;
-      };
-
-      summary.suiteNestingLevel = calculateNestingLevel(suiteOrSpec);
-
-      var children = suiteOrSpec.children();
-      for (var i = 0; i < children.length; i++) {
-        summary.children.push(this.summarize_(children[i]));
+    var i;
+    if (numErrors > 0) {
+      this.testOutput += '<div class="spec failing">' + result.description + '</div>';
+      for (i = 0; i < numErrors; i++) {
+       this.testOutput += '<div class="spec-message failing">' + result.failedExpectations[i].message + '</div>';
       }
-    }
-
-    return summary;
-  },
-
-  // This is heavily influenced by Jasmine's Html/Trivial Reporter
-  reportRunnerResults: function(runner) {
-    var results = runner.results();
-
-    var specs = runner.specs();
-    var specCount = specs.length;
-
-    this.finished_ = true;
-    if(this.callback_) { this.callback_(runner); }
-  },
-
-  reportSuiteResults: function(suite) {
-    // Not used in this context
-  },
-
-  reportSpecResults: function(spec) {
-    var result = spec.results();
-    var msg = '';
-    if (!result.skipped && !result.passed()) {
-      this.addFailureToFailures_(spec);
+    } else {
+      this.testOutput += '<div class="spec passing">' + result.description + '</div>';
     }
   },
 
-  addFailureToFailures_: function(spec) {
-    var result = spec.results();
-    var failureItem = null;
-
-    var items_length = result.items_.length;
-    for (var i = 0; i < items_length; i++) {
-      if (result.items_[i].passed_ === false) {
-        failureItem = result.items_[i];
-
-        var failure = {
-          spec: spec.suite.getFullName() + " " + spec.description,
-          message: failureItem.message,
-          stackTrace: failureItem.trace.stack
-        };
-
-        this.failures_.push(failure);
-        if (this.specMap_[spec.description].errors.length === 0) {
-          this.numFailingSpecs_ += 1;
-        }
-        this.specMap_[spec.description].errors.push(failure);
-      }
+  jasmineDone: function() {
+    this.testOutput += '<div class="pb"></div>';
+    var testPercent = this.numPassingSpecs / this.numSpecs;
+    this.testsStr = this.numPassingSpecs + '/' + this.numSpecs;
+    this.testClass = 'poor';
+    if (testPercent > 0.85) {
+      this.testClass = 'great';
+    } else if (testPercent > 0.5) {
+      this.testClass = 'good';
     }
-  },
-
-  printRunnerResults_: function(runner){
-    var results = runner.results();
-    var specs = runner.specs();
   }
 };
 
